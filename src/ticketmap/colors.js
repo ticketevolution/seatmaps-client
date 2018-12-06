66158ec23917f807cde11040e9d8282b76e83cc6
Joseph Spens <jpspens@gmail.com>
2018-12-06 15:05:34 -0500

    Query all elements with a given section ID

--- src/ticketmap/colors.js
+++ src/ticketmap/colors.js
@@ -4,10 +4,7 @@ export const fillSection = (id, color, type = 'fill') => {
   if (!color) {
     throw Error('Color is undefined for this section.');
   }
-  const elem = document.querySelector(`[data-section-id="${id}"]`);
-  if (elem.nodeName === 'path') {
-    elem.setAttribute(type, color);
-  } else {
-    elem.querySelectorAll('path').forEach(path => path.setAttribute(type, color));
-  }
+  Array.from(document.querySelectorAll(`[data-section-id="${id}"]`))
+    .reduce((memo, element) => [...memo, element, ...element.querySelectorAll('path')], [])
+    .forEach(element => element.setAttribute(type, color));
 }
