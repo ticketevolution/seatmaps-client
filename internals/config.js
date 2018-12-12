cb61f123825327967c367e2849b5178e8763ae72
Maya Vera <maya@ticketevolution.com>
2018-12-12 12:35:47 -0500

    removed dll

--- internals/config.js
+++ internals/config.js
@@ -1,37 +1,16 @@
-const resolve = require('path').resolve
 const pullAll = require('lodash/pullAll')
 const uniq = require('lodash/uniq')
 
 const seatmaps = {
   version: '0.0.1',
-  dllPlugin: {
-    defaults: {
-      /**
-       * we need to exclude dependencies which are not intended for the browser
-       * by listing them here.
-       */
-      exclude: ['chalk', 'compression', 'cross-env', 'express', 'ip', 'minimist', 'body-parser', 'bluebird', 'unirest'],
 
-      /**
-       * Specify any additional dependencies here. We include core-js and lodash
-       * since a lot of our dependencies depend on them and they get picked up by webpack.
-       */
-      include: ['eventsource-polyfill', 'babel-polyfill'],
+  entry(pkg) {
+    const dependencyNames = Object.keys(pkg.dependencies)
+    const includeDependencies = uniq(dependencyNames.concat(include))
 
-      // The path where the DLL manifest and bundle will get built
-      path: resolve('../node_modules/seatmaps-dlls'),
-    },
-
-    entry(pkg) {
-      const dependencyNames = Object.keys(pkg.dependencies)
-      const exclude = pkg.dllPlugin.exclude || seatmaps.dllPlugin.defaults.exclude
-      const include = pkg.dllPlugin.include || seatmaps.dllPlugin.defaults.include
-      const includeDependencies = uniq(dependencyNames.concat(include))
-
-      return {
-        seatmapsDeps: pullAll(includeDependencies, exclude),
-      }
-    },
+    return {
+      seatmapsDeps: pullAll(includeDependencies, exclude),
+    }
   },
 }
 
--- internals/webpack/webpack.dev.babel.js
+++ internals/webpack/webpack.dev.babel.js
@@ -1,32 +1,6 @@
 const path = require('path')
-const fs = require('fs')
 const webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-const AddAssetHtmlPlugin = require('add-asset-html-webpack-plugin')
-const glob = require('glob')
-const pkg = require(path.resolve(process.cwd(), 'package.json'))
-const dllPlugin = pkg.dllPlugin
-
-const plugins = [
-  new webpack.HotModuleReplacementPlugin(), // Tell webpack we want hot reloading
-  new HtmlWebpackPlugin({
-    title: 'Ticket Evolution Map',
-    filename: 'index.html',
-    inject: true,
-    template: 'src/index.html',
-  }),
-]
-
-if (dllPlugin) {
-  glob.sync(`${dllPlugin.path}/*.dll.js`).forEach(dllPath => {
-    plugins.push(
-      new AddAssetHtmlPlugin({
-        filepath: dllPath,
-        includeSourcemap: false,
-      })
-    )
-  })
-}
 
 module.exports = require('./webpack.base.babel')({
   entry: ['babel-polyfill', path.join(process.cwd(), 'src/index.js')],
@@ -39,7 +13,15 @@ module.exports = require('./webpack.base.babel')({
   mode: 'development',
 
   // Add development plugins
-  plugins: dependencyHandlers().concat(plugins),
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(), // Tell webpack we want hot reloading
+    new HtmlWebpackPlugin({
+      title: 'Ticket Evolution Map',
+      filename: 'index.html',
+      inject: true,
+      template: 'src/index.html',
+    }),
+  ],
 
   // Emit a source map for easier debugging
   devtool: 'source-map',
@@ -48,61 +30,3 @@ module.exports = require('./webpack.base.babel')({
     hints: false,
   },
 })
-
-/**
- * Select which plugins to use to optimize the bundle's handling of
- * third party dependencies.
- *
- * If there is a dllPlugin key on the project's package.json, the
- * Webpack DLL Plugin will be used.  Otherwise the CommonsChunkPlugin
- * will be used.
- *
- */
-function dependencyHandlers() {
-  // Don't do anything during the DLL Build step
-  if (process.env.BUILDING_DLL) {
-    return []
-  }
-
-  const dllPath = path.resolve(process.cwd(), dllPlugin.path || 'node_modules/seatmaps-dlls')
-
-  /**
-   * If DLLs aren't explicitly defined, we assume all production dependencies listed in package.json
-   * Reminder: You need to exclude any server side dependencies by listing them in dllConfig.exclude
-   */
-  if (!dllPlugin.dlls) {
-    const manifestPath = path.resolve(dllPath, 'seatmapsDeps.json')
-
-    if (!fs.existsSync(manifestPath)) {
-      console.error('The DLL manifest is missing. Please run `npm run build:dll`')
-      process.exit(0)
-    }
-
-    return [
-      new webpack.DllReferencePlugin({
-        context: process.cwd(),
-        manifest: require(manifestPath),
-      }),
-    ]
-  }
-
-  // If DLLs are explicitly defined, we automatically create a DLLReferencePlugin for each of them.
-  const dllManifests = Object.keys(dllPlugin.dlls).map(name => path.join(dllPath, `/${name}.json`))
-
-  return dllManifests.map(manifestPath => {
-    if (!fs.existsSync(path)) {
-      if (!fs.existsSync(manifestPath)) {
-        console.error(`The following Webpack DLL manifest is missing: ${path.basename(manifestPath)}`)
-        console.error(`Expected to find it in ${dllPath}`)
-        console.error('Please run: npm run build:dll')
-
-        process.exit(0)
-      }
-    }
-
-    return new webpack.DllReferencePlugin({
-      context: process.cwd(),
-      manifest: require(manifestPath),
-    })
-  })
-}
