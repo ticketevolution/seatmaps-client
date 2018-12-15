2d570c47127cbd3e590ba8f30372260557474f55
Maya Vera <maya@mayavera.me>
2018-12-14 21:42:50 -0500

    fixed compression options

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -6,14 +6,12 @@ export default merge(base, {
   mode: 'production',
   plugins: [
     new CompressionPlugin({
-      asset: '[path].gz[query]',
-      algorithm: 'gzip',
       test: /\.js$|\.css$|\.html$/,
-      threshold: 10240,
-      minRatio: 0.8,
-    }),
+      cache: true,
+      minRatio: 0.8
+    })
   ],
   performance: {
-    assetFilter: assetFilename => !/(\.map$)|(^(main\.|favicon\.))/.test(assetFilename),
+    assetFilter: assetFilename => !/(\.map$)|(^(main\.|favicon\.))/.test(assetFilename)
   }
 })
