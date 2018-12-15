82058e3ea998c3ab994887da8c5d136803399def
Maya Vera <maya@mayavera.me>
2018-12-14 23:47:22 -0500

    moved example code from src to examples/using-module

---
+++ examples/using-module/main.js
@@ -0,0 +1,6 @@
+import Tevomaps from '../../src/index'
+
+window.seatmap = new Tevomaps({
+  venueId: '896',
+  configurationId: '14341'
+}).build('map')
--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,9 +1,26 @@
 import CompressionPlugin from 'compression-webpack-plugin'
 import merge from 'webpack-merge'
+import path from 'path'
 import base from './webpack.config.base.babel'
 
 export default merge(base, {
   mode: 'production',
+  entry: [
+    'unfetch/polyfill',
+    path.join(__dirname, 'src', 'index.js')
+  ],
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    publicPath: '/',
+    library: {
+      root: 'Tevomaps',
+      amd: 'ticket-evolution-seatmaps',
+      commonjs: 'ticket-evolution-seatmaps'
+    },
+    libraryTarget: 'umd',
+    filename: 'tevomaps.js',
+    chunkFilename: '[name].chunk.js'
+  },
   plugins: [
     new CompressionPlugin({
       test: /\.js$|\.css$|\.html$/,
