5aba3440e97eb7eb666fb838ea8f257c17abbfd6
Maya Vera <maya@ticketevolution.com>
2018-12-18 17:45:58 -0500

    zoom and legend are now floating above map content

--- src/ticketmap/Legend.js
+++ src/ticketmap/Legend.js
@@ -2,8 +2,8 @@ import { h, Component } from 'preact'
 import classnames from 'classnames'
 import c from './Legend.scss'
 
-const Swatch = ({color, name, ...props}) => (
-  <div class={c.swatch} style={{ backgroundColor: color }}/>
+const Swatch = ({ color, name, ...props }) => (
+  <div class={c.swatch} style={{ backgroundColor: color }} />
 )
 
 export default class Legend extends Component {
@@ -22,17 +22,17 @@ export default class Legend extends Component {
 
   render() {
     const { open } = this.state
-    const { options } = this.props
+    const { options, className } = this.props
 
     return (
-      <div className={classnames(c.legend, open && c.open)}>
+      <div className={classnames(c.legend, open && c.open, className)}>
         <div className={c.toggle} onClick={() => this.setState({ open: !open })}>
           <span>{open ? 'Hide' : 'Show'} Legend</span>
         </div>
         <div className={c.options}>
           {options.map(option => (
             <div>
-              <Swatch color={option.color}/>
+              <Swatch color={option.color} />
               <span>{option.name}</span>
             </div>
           ))}
