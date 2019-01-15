45645dc8e8419d13811b64137406698ff6581db7
Maya Vera <maya@ticketevolution.com>
2019-01-15 11:28:58 -0500

    removed missing ticket from example

--- examples/using-module/main.js
+++ examples/using-module/main.js
@@ -4,8 +4,5 @@ import ticketGroups from './ticket-groups-1591449'
 window.seatmap = new Tevomaps({
   venueId: '896',
   configurationId: '14341',
-  ticketGroups: [
-    ...ticketGroups,
-    { tevo_section_name: 'foo' }
-  ]
+  ticketGroups
 }).build('map')
