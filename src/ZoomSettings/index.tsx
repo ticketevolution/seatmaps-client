7c9af9ddb9f39bfb7b29ae6dec85382034895790
Joseph Spens <jpspens@gmail.com>
2019-03-07 14:55:22 -0500

    SEAT-165 Decrease zoom sensitivity on seatmap client

--- src/ZoomSettings/index.tsx
+++ src/ZoomSettings/index.tsx
@@ -20,7 +20,6 @@ export default class ZoomSettings extends Component<Props> {
     super(props)
 
     this.mapZoom = svgPanZoom(this.props.mapSvg, {
-      zoomScaleSensitivity: 0.8,
       minZoom: 0.8,
     })
   }
