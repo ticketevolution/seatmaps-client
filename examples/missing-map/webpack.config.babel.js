351f760d86b8cecec42104baeb1b3114b211d767
Maya Vera <maya@mayavera.me>
2019-03-07 11:33:15 -0500

    created example for missing map (#111)

---
+++ examples/missing-map/webpack.config.babel.js
@@ -0,0 +1,59 @@
+import HtmlWebpackPlugin from 'html-webpack-plugin'
+import merge from 'webpack-merge'
+import path from 'path'
+import base from '../../webpack.config.base.babel'
+
+export default merge(base, {
+  mode: 'development',
+  devtool: 'source-map',
+  entry: [
+    'unfetch/polyfill',
+    path.join(__dirname, 'main.js'),
+    path.join(__dirname, 'main.css')
+  ],
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Seatmaps | Missing Map Example',
+      template: 'index.html',
+      meta: {
+        viewport: 'width=device-width, initial-scale=1'
+      }
+    })
+  ],
+  performance: {
+    hints: false
+  },
+  optimization: {
+    splitChunks: {
+      chunks: 'all'
+    }
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx', '.json']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        use: {
+          loader: 'html-loader',
+          options: {
+            minimize: true
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true
+            }
+          }
+        ]
+      }
+    ]
+  }
+})
