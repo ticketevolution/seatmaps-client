8f52a39bc7336204b9bb14ded50645a5fb5fd3f2
Maya Vera <maya@mayavera.me>
2019-03-20 16:35:00 -0400

    isolated functions for tooltip style, position, and rendered direction logic

--- src/Tooltip/index.tsx
+++ src/Tooltip/index.tsx
@@ -24,6 +24,11 @@ interface DefaultProps {
   name: string
 }
 
+interface Point {
+  x: number
+  y: number
+}
+
 export default class Tooltip extends Component<Props & DefaultProps> {
   container = React.createRef<HTMLDivElement>()
 
@@ -36,92 +41,154 @@ export default class Tooltip extends Component<Props & DefaultProps> {
     color: '#000000'    
   }
 
-  render () {
-    const { isActive, ticketGroups, x, y, name, color } = this.props
-    const prices = ticketGroups.map(ticketGroup => ticketGroup.price).sort((a, b) => a - b)
+  direction(): string[] {
+    const container = this.container.current
+    const { x, y } = this.props
+    const direction = ['up', 'right']
 
-    const containerStyle: CSSProperties = {
+    if (!container || !container.parentElement) {
+      return direction
+    }
+
+    if (container.parentElement) {
+      // display to the left of the cursor when the right side of the tooltip is clipped
+      if (x + container.clientWidth > container.parentElement.clientWidth) {
+        direction[1] = 'left'
+      }
+
+      // display beneath the cursor when the top of the tooltip is clipped
+      if (y < container.clientHeight) {
+        direction[0] = 'down'
+      }
+    }
+
+    return direction
+  }
+  
+  position(): Point {
+    const direction = this.direction()
+    const point = {
+      x: this.props.x,
+      y: this.props.y
+    }
+
+    const container = this.container.current
+    if (container) {
+      if (direction[0] === 'up') {
+        point.y -= container.clientHeight
+      }
+
+      if (direction[1] === 'left') {
+        point.x -= container.clientWidth
+      }
+    }
+
+    return point
+  }
+
+  tipStyle(): CSSProperties {
+    const direction = this.direction()
+
+    const style: CSSProperties = {
+      width: 0,
+      height: 0,
+      borderStyle: 'solid'
+    }
+
+    if (direction[0] === 'down') {
+      if (direction[1] === 'right') {
+        style.borderWidth = '10px 0 0 10px'
+        style.borderColor = 'transparent transparent transparent white'
+      } else {
+        style.borderWidth = '0 0 10px 10px'
+        style.borderColor = 'transparent transparent white transparent'
+      }
+    } else {
+      if (direction[1] === 'right') {
+        style.borderWidth = '10px 10px 0 0'
+        style.borderColor = 'white transparent transparent transparent'
+      } else {
+        style.borderWidth = '0 10px 10px 0'
+        style.borderColor = 'transparent white transparent transparent'
+      }
+    }
+
+    return style
+  }
+
+  containerStyle(): CSSProperties {
+    const { isActive } = this.props
+    const direction = this.direction()
+    const position = this.position()
+
+    return {
       position: 'absolute',
       zIndex: 1,
       transition: 'top .1s, left .1s',
       opacity: isActive ? 1 : 0,
       padding: 5,
       display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'flex-start',
+      flexDirection: direction[0] === 'down' ? 'column' : 'column-reverse',
+      alignItems: direction[1] === 'right' ? 'flex-start' : 'flex-end',
       filter: 'drop-shadow(rgba(0, 0, 0, 0.5) 0 2px 2px)',
-      pointerEvents: 'none'
+      pointerEvents: 'none',
+      top: position.y,
+      left: position.x
     }
+  }
 
-    let renderAboveTarget = true
-    let renderRightOfTarget = true
-
-    const container = this.container.current
-
-    if (container && x !== undefined && y !== undefined) {
-      if (container.parentElement && x + container.clientWidth > container.parentElement.clientWidth) {
-        renderRightOfTarget = false
-      }
-
-      if (y - container.clientHeight < 0) {
-        renderAboveTarget = false
-      }
-
-      if (renderAboveTarget) {
-        containerStyle.top = y - container.clientHeight
-      } else {
-        containerStyle.top = y
-      }
-
-      if (renderRightOfTarget) {
-        containerStyle.left = x
-      } else {
-        containerStyle.left = x - container.clientWidth
-        containerStyle.alignItems = 'flex-end'
-      }
+  swatchStyle(): CSSProperties {
+    return {
+      width: 10,
+      height: 10,
+      backgroundColor: this.props.color,
+      display: 'inline-block',
+      marginRight: 5
     }
+  }
 
-    const tipStyle = {
-      width: 0,
-      height: 0,
-      borderStyle: 'solid'
+  contentStyle(): CSSProperties {
+    return {
+      backgroundColor: 'white',
+      padding: 20      
     }
+  }
+
+  nameStyle(): CSSProperties {
+    return {
+      display: 'flex',
+      alignItems: 'center'
+    }
+  }
+
+  priceStyle(): CSSProperties {
+    return {
+      fontWeight: 700
+    }
+  }
+
+  render () {
+    const prices = this.props.ticketGroups
+      .map(ticketGroup => ticketGroup.price)
+      .sort((a, b) => a - b)
 
     return (
-      <div ref={this.container} style={containerStyle}>
-        {!renderAboveTarget && <div style={{
-          ...tipStyle,
-          borderWidth: renderRightOfTarget ? '10px 0 0 10px' : '0 0 10px 10px',
-          borderColor: renderRightOfTarget ? 'transparent transparent transparent white' : 'transparent transparent white transparent'
-        }} /> }
-        <div style={{
-          backgroundColor: 'white',
-          padding: 20
-        }}>
-          <div style={{
-            display: 'flex',
-            alignItems: 'center'
-          }}>
-            <div style={{
-              width: 10,
-              height: 10,
-              backgroundColor: color,
-              display: 'inline-block',
-              marginRight: 5
-            }} />
-            {name}
+      <div ref={this.container} style={this.containerStyle()}>
+        <div style={this.tipStyle()}/>
+        <div style={this.contentStyle()}>
+          <div style={this.nameStyle()}>
+            <div style={this.swatchStyle()}/>
+            {this.props.name}
           </div>
           <div>
-            {prices.length} listing{prices.length !== 1 ? 's' : ''}
+            {this.props.ticketGroups.length} listing{prices.length !== 1 ? 's' : ''}
             {' ● '}
-            Starting at <span style={{ fontWeight: 700 }}>{formatCurrency(prices[0])}</span>
+            Starting at{' '}
+            <span style={this.priceStyle()}>
+              {formatCurrency(prices[0])}
+            </span>
           </div>
         </div>
-        {renderAboveTarget && <div style={{
-          ...tipStyle,
-          borderWidth: renderRightOfTarget ? '10px 10px 0 0' : '0 10px 10px 0',
-          borderColor: renderRightOfTarget ? 'white transparent transparent transparent' : 'transparent white transparent transparent'
-        }} /> }
       </div>
     )
   }
