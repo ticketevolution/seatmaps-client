61231123d9a7c90c837d92e99e9e1d8801851878
Maya Vera <maya@mayavera.me>
2019-03-20 13:36:10 -0400

    added tests for isActive effects

--- src/Tooltip/tooltip.test.tsx
+++ src/Tooltip/tooltip.test.tsx
@@ -6,4 +6,18 @@ describe('Tooltip', () => {
   it('renders', () => {
     expect(shallow(<Tooltip />).exists()).toBe(true)
   })
+
+  it('renders a transparent element when isActive is false', () => {
+    const tooltip = shallow(<Tooltip isActive={false} />)
+
+    expect(tooltip.exists()).toBeTruthy()
+    expect(tooltip.getElement().props.style.opacity).toEqual(0)
+  })
+
+  it('renders an opaque element when isActive is true', () => {
+    const tooltip = shallow(<Tooltip isActive />)
+
+    expect(tooltip.exists()).toBeTruthy()
+    expect(tooltip.getElement().props.style.opacity).toEqual(1)
+  })
 })
