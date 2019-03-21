98a8974e883c45f105d8a29a422ef10d558364fd
Maya Vera <maya@mayavera.me>
2019-03-21 14:07:18 -0400

    simplified swatch unit tests

--- src/Legend/swatch.test.tsx
+++ src/Legend/swatch.test.tsx
@@ -1,20 +1,12 @@
-import React from 'react'
-import { shallow, ShallowWrapper } from 'enzyme'
+import 'jest-enzyme'
 
-import Swatch, { Props } from './swatch'
+import React from 'react'
+import { shallow } from 'enzyme'
+
+import Swatch from './swatch'
 
 describe('Swatch', () => {
-  let wrapper: ShallowWrapper<Props>
-  let props: Props
-
-  beforeEach(() => {
-    props = {
-      color: '#c00'
-    }
-    wrapper = shallow(<Swatch {...props} />)
-  })
-
-  it('renders without a style prop', () => {
-    expect(wrapper.exists()).toBe(true)
+  it('renders', () => {
+    expect(shallow(<Swatch color='#000' />)).toExist()
   })
 })
