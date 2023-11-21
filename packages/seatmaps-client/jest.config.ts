64a4406f2593198b0e14ccb8ef0fb7d982192335
Keith River <keith.river@gmail.com>
2023-11-21 17:34:25 +0100

    SEAT-285: repo clean up chore: package build configs clean up tests: extracted mock-data into a separate package

---
+++ packages/seatmaps-client/jest.config.ts
@@ -0,0 +1,64 @@
+import { Config } from "jest";
+
+module.exports = {
+  testMatch: [
+    "**/__tests__/**/*test.[jt]s?(x)",
+    "**/?(*.)+(spec|test).[jt]s?(x)",
+  ],
+  clearMocks: true,
+  collectCoverageFrom: ["!src/declarations", "**/*.{ts,tsx}"],
+  coverageDirectory: "coverage",
+  coverageThreshold: {
+    global: {
+      branches: 90,
+      functions: 90,
+      lines: 90,
+      statements: 90,
+    },
+  },
+  moduleNameMapper: {
+    ".+\\.(svg|png|jpg)$": "identity-obj-proxy",
+  },
+  setupFilesAfterEnv: [
+    "<rootDir>/node_modules/jest-enzyme/lib/index.js",
+    "<rootDir>/src/__tests__/setupTests.ts",
+  ],
+  testEnvironment: "jsdom",
+  transform: {
+    "\\.(js|mjs|ts)$": ["@swc/jest", {}],
+    "^.+\\.jsx?$": [
+      "@swc/jest",
+      {
+        jsc: {
+          parser: {
+            syntax: "ecmascript",
+            jsx: true,
+            decorators: false,
+            dynamicImport: false,
+          },
+        },
+      },
+    ],
+    "^.+\\.tsx?$": [
+      "@swc/jest",
+      {
+        jsc: {
+          parser: {
+            syntax: "typescript",
+            jsx: true,
+            decorators: false,
+            dynamicImport: false,
+          },
+          transform: {
+            react: {
+              runtime: "automatic",
+            },
+            hidden: {
+              jest: true,
+            },
+          },
+        },
+      },
+    ],
+  },
+} satisfies Config;
--- packages/seatmaps-client/tsup.config.esm.ts
+++ packages/seatmaps-client/tsup.config.esm.ts
@@ -1,51 +1,36 @@
 import { readFileSync } from "node:fs";
 import { defineConfig } from "tsup";
-import { umdWrapper } from "esbuild-plugin-umd-wrapper";
-import svgr from 'esbuild-plugin-svgr'
-import jsx from '@svgr/plugin-jsx';
-import { name, dependencies } from "./package.json";
-
-const NODE_ENV = 'production' || process.env.NODE_ENV;
+import svgr from "esbuild-plugin-svgr";
 
 export default defineConfig({
-    // replaceNodeEnv: true,
-    entryPoints: ['src/index.tsx'],
-    // entry: ['src/**/[^.]*.ts(x)'],
-    outDir: './esm',
-    target: 'es2020',
-    splitting: false,
-    treeshake: true,
-    metafile: true,
-    clean: true,
-    dts: false,
-    minify: false,
-    bundle: true,
-    // watch: NODE_ENV === 'development',
+  entryPoints: ["src/index.tsx"],
+  outDir: "./esm",
+  target: "es2020",
+  splitting: false,
+  treeshake: true,
+  metafile: true,
+  clean: true,
+  dts: false,
+  minify: false,
+  keepNames: true,
+  bundle: true,
   skipNodeModulesBundle: true,
   sourcemap: true,
-//   esbuildOptions: (options) => {
-//     console.log(options.entryPoints)
-//     if (Array.isArray(options.entryPoints)) {
-//         options.entryPoints = options.entryPoints.filter((item) => typeof item === 'string' ? !item.includes('.test.') : !item.in.includes('.test.')) as string[] | { in: string; out: string; }[]
-//     }
-//     // options.entryPoints?.filter((item) => typeof item === 'string' ? item.includes('.test.') : item)
-//   },
   esbuildPlugins: [
     svgr(),
     {
-  name: 'excludeVendorFromSourceMap',
-  setup(build) {
-    build.onLoad({ filter: /node_modules/ }, args => {
-      return {
-        contents: readFileSync(args.path, 'utf8')
-          + '\n//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIiJdLCJtYXBwaW5ncyI6IkEifQ==',
-        loader: 'default',
-      }
-    })
-  },
-}
+      name: "excludeVendorFromSourceMap",
+      setup(build) {
+        build.onLoad({ filter: /node_modules/ }, (args) => {
+          return {
+            contents:
+              readFileSync(args.path, "utf8") +
+              "\n//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIiJdLCJtYXBwaW5ncyI6IkEifQ==",
+            loader: "default",
+          };
+        });
+      },
+    },
   ],
-//   external: [/\.test\./],
-//   noExternal: Object.keys(dependencies),
-  format: ['esm']
+  format: ["esm"],
 });
