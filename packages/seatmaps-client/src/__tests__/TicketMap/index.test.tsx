eadc7c7d0578143123862675c52cd02e314531b3
bsl782 <brucelee92371@gmail.com>
2024-01-17 00:26:50 +0900

    Fixed tests

--- packages/seatmaps-client/src/__tests__/TicketMap/index.test.tsx
+++ packages/seatmaps-client/src/__tests__/TicketMap/index.test.tsx
@@ -90,7 +90,7 @@ describe("TicketMap", () => {
       render(<TicketMap {...dataProps} />);
     });
     expect(
-      await screen.findByText("Seating chart not available."),
+      await screen.findByText("Seating Chart Coming Soon"),
     ).toBeInTheDocument();
     expect(consoleError).toHaveBeenCalledTimes(1);
     consoleError.mockRestore();
