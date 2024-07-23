3d14f60d56d936ea3b89b5f65deba184a0671739
harmantevo <harman.singh-c@victorylive.com>
2024-07-23 18:56:59 +0530

    added example and changesets too

--- packages/seatmaps-client/vite.config.ts
+++ packages/seatmaps-client/vite.config.ts
@@ -1,26 +1,30 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { resolve } from 'path';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
+import inject from "@rollup/plugin-inject";
 
 export default defineConfig({
   plugins: [react()],
   build: {
     lib: {
-      entry: resolve(__dirname, 'src/index.tsx'),
-      name: 'seatMaps',
+      entry: resolve(__dirname, "src/index.tsx"),
+      name: "Tevomaps", // Global variable name for your library
       fileName: () => `bundle.js`,
-      
+      formats: ["umd"],
     },
     rollupOptions: {
-      external: ['react', 'react-dom', 'lodash.union', 'lodash.pick'],
+      external: [],
       output: {
-        globals: {
-          react: 'React',
-          'react-dom': 'ReactDOM',
-          'lodash.union': 'union',
-          'lodash.pick': 'pick',
-        },
+        globals: {},
+        name: "Tevomaps", // Ensure this matches the global variable
+        format: "umd", // Ensure the esm format
+        exports: "named", // Explicitly specify named exports
       },
+      plugins: [
+        inject({
+          process: "process/browser",
+        }),
+      ],
     },
   },
   server: {
