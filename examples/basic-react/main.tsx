78b1b6d7f00a2ee007cd103b36aff6b422112cd3
mohumofu3 <mohumofu3@gmail.com>
2024-10-08 19:20:35 +0900

    ticketevolution -> aisaac-lab

--- examples/basic-react/main.tsx
+++ examples/basic-react/main.tsx
@@ -1,18 +1,18 @@
-import {createRoot } from 'react-dom/client';
-import { TicketMap } from '@ticketevolution/seatmaps-client'
-import ticketGroups from '@-/mock-data/data/ticket-groups-1591449.json'
+import { createRoot } from "react-dom/client";
+import { TicketMap } from "@aisaac-lab/seatmaps-client";
+import ticketGroups from "@-/mock-data/data/ticket-groups-1591449.json";
 
-const rootEl = document.getElementById('map');
+const rootEl = document.getElementById("map");
 if (!rootEl) {
-  throw new Error("no root element in html")
+  throw new Error("no root element in html");
 }
 
 const root = createRoot(rootEl);
 
-root.render((
-  <TicketMap 
-    venueId='896'
-    configurationId='14341'
+root.render(
+  <TicketMap
+    venueId="896"
+    configurationId="14341"
     ticketGroups={ticketGroups}
-  />
-));
+  />,
+);
