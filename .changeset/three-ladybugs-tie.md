edeaa09ae8e4698a163ee8281be363f6a68c1d56
SrujanaSaka20 <srujana.saka-c@victorylive.com>
2025-01-10 14:55:25 +0530

    Ntrfcs 42 : Zoom, Panning and Mobile click issue (#299)
    * restrict infinity zooming and panning, click issue in mobile view
    
    * changeset changes
    
    * fixed lint issues and testcase issues

---
+++ .changeset/three-ladybugs-tie.md
@@ -0,0 +1,5 @@
+---
+"@ticketevolution/seatmaps-client": patch
+---
+
+Fix mobile click issues and restrict infinite zooming and panning behavior. Limit zoom in to 10% of the original size and prevent zooming out beyond the original size.
--- packages/seatmaps-client/src/TicketMap/index.tsx
+++ packages/seatmaps-client/src/TicketMap/index.tsx
@@ -461,11 +461,19 @@ export class TicketMap extends Component<Props & DefaultProps, State> {
    * Interaction Callbacks
    */
 
-  onMouseOver = ({ clientX, clientY, target }: React.MouseEvent<HTMLElement>) =>
+  onMouseOver = ({
+    clientX,
+    clientY,
+    target,
+  }: React.MouseEvent<HTMLElement>) => {
+    if (this.state.isTouchDevice) return;
     this.doHover(target as HTMLElement, clientX, clientY);
+  };
 
-  onMouseOut = ({ relatedTarget }: React.MouseEvent<HTMLElement>) =>
+  onMouseOut = ({ relatedTarget }: React.MouseEvent<HTMLElement>) => {
+    if (this.state.isTouchDevice) return;
     this.doHoverCleanup(relatedTarget as HTMLElement);
+  };
 
   onMouseMove = ({ nativeEvent }: React.MouseEvent<HTMLElement>) =>
     this.setState({
@@ -475,15 +483,29 @@ export class TicketMap extends Component<Props & DefaultProps, State> {
 
   onClick = () => this.doSelect();
 
+  onTouchStart = (e: React.TouchEvent<HTMLElement>) => {
+    const section = this.getSectionFromTarget(e.target as HTMLElement);
+    if (section) {
+      this.setState({
+        currentHoveredSection: section,
+      });
+      this.highlightSection(section);
+    }
+  };
+
   onTouchMove = () => {
     this.setState({ dragging: true });
   };
 
   onTouchEnd = (e: React.TouchEvent<HTMLElement>) => {
+    const section = this.state.currentHoveredSection;
+    if (section) {
+      this.doSelect(section);
+      this.setState({ currentHoveredSection: undefined });
+    }
     if (this.state.dragging) {
       e.preventDefault();
     }
-
     this.setState({ dragging: false });
   };
 
@@ -602,6 +624,7 @@ export class TicketMap extends Component<Props & DefaultProps, State> {
         onMouseOut={this.onMouseOut}
         onMouseMove={this.onMouseMove}
         onClick={this.onClick}
+        onTouchStart={this.onTouchStart}
         onTouchEnd={this.onTouchEnd}
         onTouchMove={this.onTouchMove}
         style={{
